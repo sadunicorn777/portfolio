4214ab535943661a165256713deaaa1ba7dcc24b
sadunicorn777 <114984464+sadunicorn777@users.noreply.github.com>
2022-12-13 03:08:12 -0500

    Create ContactBox.js

---
+++ src/body/ContactBox.js
@@ -0,0 +1,23 @@
+import '../App.css';
+
+export const ContactBox = () => {
+
+    return (
+        <div className="contact-box">
+            <div className="contact-box-tape" />
+            <div className="contact-box-title">
+                Contact
+            </div>
+            <div className="contact-box-text">
+                This site was designed, illustrated and coded by me. If 
+you are interested in a creative commission please 
+contact me at [REDACTED], preferably 
+with “Commission” in the subject line. 
+
+For anything else, don’t hesitate to get in touch 
+by emailing me at [REDACTED]!
+            </div>
+        </div>
+
+    )
+}
