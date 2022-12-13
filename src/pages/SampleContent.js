1c9b57f87e883bffb8294ba157d965613b7677c9
sadunicorn777 <114984464+sadunicorn777@users.noreply.github.com>
2022-12-13 03:13:12 -0500

    Create SampleContent.js

---
+++ src/pages/SampleContent.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+export const SampleContent = () => {
+
+    return (
+       <div className="post-style">
+           This is a sample post content. You can use <b>bold</b>, <i>italic</i>, and <u>underline</u>. You can also use them <b><i><u>at the same time.</u></i></b>
+           <p><h>This is what a header looks like.</h></p>
+           <p>You can also include images but don't forget to style them so they are the right size.</p>
+           <p>Happy posting!</p>
+        </div>
+    )
+}
